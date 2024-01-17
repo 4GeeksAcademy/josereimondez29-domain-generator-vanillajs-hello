800300e7c2774f06458e2faaf82a9702aeecf43c
Jose Reimondez <josereimondez29@gmail.com>
2024-01-17 18:53:57 +0000

    domain generator finish

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//write your code here
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let extensions = [".com", ".net", ".us", ".io"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l < extensions.length; l++) {
+        let domain = pronoun[i] + adj[j] + noun[k] + extensions[l];
+        console.log(domain);
+      }
+    }
+  }
+}
